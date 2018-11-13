9dd54af70a7c51b9ddfe8da2bfe609add0d049e9
Gio Palacino <gpalacino@staywell.com>
2018-11-13 10:54:43 -0600

    Used stringify safe for error

--- index.js
+++ index.js
@@ -48,7 +48,8 @@ class MotivPlatform {
           self.motivData.isAwake = wokeTime <= now;
         })
         .catch((err) => {
-          this.log.error('Failed to update isAwake status: %s', JSON.stringify(err));
+          var stringify = require('json-stringify-safe');
+          this.log.error('Failed to update isAwake status: %s', stringify(err));
         });
     } catch (err) {
       this.log.error(err);
