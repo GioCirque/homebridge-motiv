8195719a2bc340825c34371f8993abf2beaa269b
Gio Palacino <gpalacino@staywell.com>
2018-11-27 12:56:51 -0600

    Splitting code

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,4 +1,10 @@
-import { timeSpanString } from '../lib/utils';
+import {
+  timeSpanString,
+  toTitleCase,
+  getUTCDate,
+  getUTCNowDate,
+  splitDateAndTime,
+} from '../lib/utils';
 
 describe('Utility tests', () => {
   it('Should return h:mm:ss formatted time', () => {
@@ -14,5 +20,55 @@ describe('Utility tests', () => {
       new Date(1541424202 * milliseconds)
     );
     expect(timeString02).toEqual('428173:23:22');
+  });
+
+  it('Should return a split date and time roughly equal to now', () => {
+    const value = new Date();
+    const { date, time } = splitDateAndTime();
+    const parts = value.toISOString().split('T');
+    expect(date).toEqual(parts[0]);
+    expect(
+      time
+        .split(':')
+        .slice(0, 2)
+        .join(':')
+    ).toEqual(
+      parts[1]
+        .split(':')
+        .slice(0, 2)
+        .join(':')
+    );
+  });
+
+  it('Should return a split date and time equal to the input', () => {
+    const parts = ['2018-11-05', '19:14:15.171Z'];
+    const value = new Date(parts.join('T'));
+    const { date, time } = splitDateAndTime(value);
+    expect(date).toEqual(parts[0]);
+    expect(time).toEqual(parts[1]);
+  });
+
+  it('Should return a UTC date', () => {
+    const value = getUTCNowDate();
+    const utcDate = getUTCDate(value);
+    expect(utcDate).toEqual(value);
+  });
+
+  it('Should return a title cased value', () => {
+    const value = 'the dog ran';
+    const titled = toTitleCase(value);
+    expect(titled).toBe('The Dog Ran');
+  });
+
+  it('Should return a non-title cased value', () => {
+    const value = 'a';
+    const titled = toTitleCase(value);
+    expect(titled).toBe('a');
+  });
+
+  it('Should return an undefined value', () => {
+    const value = undefined;
+    const titled = toTitleCase(value);
+    expect(titled).toBeUndefined();
   });
 });
